3e7e9b2c53f780f9905d40daa4a4425a8042a7fe
aitor94 <martinredondoaitor@gmail.com>
2017-04-26 17:39:23 +0200

    Finalizado sin testear el ejercicio 2 practica 42

--- src/practica-42/client.c
+++ src/practica-42/client.c
@@ -91,102 +91,49 @@ int main(int argc, char** argv){
 		exit(0);
 	}
 	printf("Listen realizado\n");
-	
+	printf("Inicio bucle...\n");
 	int fd;
 	c = sizeof(struct sockaddr_in);
-	if((fd=accept(socket_desc, (struct sockaddr *)&client_in,&c))<0){
-        	perror("accept fallido...\n");
-        	exit(0);
-    	}
-	
-	printf("Ejercicio 5, pulsa para continuar:\n");
-	fgets(gets,GET,stdin);
+	int pidserver;
+	if((pidserver=fork())==0) {
+		while(1) {
+			if((fd=accept(socket_desc, (struct sockaddr *)&client_in,&c))<0){
+        			perror("accept fallido...\n");
+        			exit(0);
+    			}
+			printf("Creando hijo...\n");
+			if(fork()==0) {
+				printf("Hijo con PID->%d empezando\n");
+				close(socket_desc);
+				if( recv(fd , buffer , BUFFER , 0) < 0)
+				{
+					perror("Error al recibir mensaje");
+					exit(0);
+				}
+				int secreto = *((int*)buffer);
+				sprintf(buffer,"<%d>",secreto);
+				printf("secreto 2 -> %s\n",buffer);
 
-	if( recv(fd,buffer,BUFFER , 0) < 0)
-        {
-            perror("Error al recibir mensaje");
-            exit(0);
-        }
-
-	printf("Mensaje a enviar -> %s\n",buffer);
-
-	if( send(fd, buffer , strlen(buffer) , 0) < 0)
-        {
-            perror("Envio fallido...\n");
-            exit(0);
-        }
-	
-	close(sock);
-	close(fd);
-	close(socket_desc);
-	printf("Ejercicio 6, pulsa para continuar:\n");
-	fgets(gets,GET,stdin);
-	
-	struct sockaddr_in si_me, si_other;
-	
-	bzero ((char *) &si_me, sizeof(struct sockaddr_in));
-	bzero ((char *) &si_other, sizeof(struct sockaddr_in));
-	
-	int s, i, slen = sizeof(si_other) , recv_len;
-	
-	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
-		perror("Error al crear el socket...\n");
-		exit(0);
-    	}
-	
-	memset((char *) &si_me, 0, sizeof(si_me));
-	
-	si_me.sin_family = AF_INET;
-    	si_me.sin_port = htons(50000);
-    	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	si_other.sin_family = AF_INET;
-        si_other.sin_port = htons(PORT);
-        si_other.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1){
-        	perror("Error al hace bind...\n");
-		exit(0);
+				if( send(fd , buffer , strlen(buffer) , 0) < 0)
+				{
+					perror("Envio fallido...\n");
+					exit(0);
+				}
+				close(fd);
+				printf("Hijo con PID->%d ha terminado");
+				exit(0);
+			}
+		}
 	}
-	
-	char msg[] = "HOLA!";
-	printf("Enviando->%s",msg);
-	if (sendto(s,msg,strlen(msg), 0,(struct sockaddr *) &si_other, slen) == -1)
-        {
-		perror("Error enviando HOLA...\n");
-		exit(0);
-        }
-
-	printf("Ejercicio 7, inserta para continuar(EJECUTAR ANTES AQUI):\n");
+	printf("Pulsa para terminar:\n");
 	fgets(gets,GET,stdin);
 	
-	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
-		printf("Error al crear el socket...\n");
-		exit(0);
-        }
+	kill(pidserver,SIGINT);
+	int status;
+	waitpid(pidserver,&status,0);
 
-        memset((char *) &si_me, 0, sizeof(si_me));
+	printf("Ejercicio 3:\n");
+	fgets(gets,GET,stdin);
 
-        si_me.sin_family = AF_INET;
-        si_me.sin_port = htons(PORT2);
-        si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-
-        if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1){
-                perror("Error al hace bind...\n");
-                exit(0);
-        }
-	
-	if ((recv_len = recvfrom(s, buffer, BUFFER, 0, (struct sockaddr *) &si_other, &slen)) == -1)
-        {
-		perror("Error al recibir...\n");
-		exit(0);
-        }
-	
-	if (sendto(s, buffer,strlen(buffer), 0, (struct sockaddr*) &si_other, slen) == -1)
-        {
-                perror("Error enviando respuesta...\n");
-                exit(0);
-        }
-	
 	printf("EJERSISIO TERMINADO WEY!!!!!!!!!\n");
 }
