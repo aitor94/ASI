bf557a41e83a4b044bde57e063a9ef34919abe3f
aitor94 <martinredondoaitor@gmail.com>
2017-05-13 22:30:18 +0200

    Completado examen junio 2015 hasta ejercicio 3 inclusive

--- src/junio2015/.trazas-4/client.c
+++ src/junio2015/.trazas-4/client.c
@@ -31,6 +31,9 @@ int main(int argc, char** argv){
 	char ge[GET], buffer[BUFFER];
 	struct sockaddr_in server;
 	int socket_desc, fd, c;
+	int memoria = shmget(DNI,1024,IPC_CREAT|0666);
+	int *segmento;
+	segmento = shmat(memoria,NULL,0);
 
 	printf("Comenzar:");
 	fgets(ge,GET,stdin);
@@ -66,15 +69,16 @@ int main(int argc, char** argv){
 			printf("Error en accept\n");
 			exit(0);
 		}
-		printf("Conexion recibida........\n");
+		segmento[0]++;
+		//printf("Conexion recibida........\n");
 		char user[BUFFER];	
 		if((pid=fork())==0){
 			close(socket_desc);
 			int intentos = 0;//gcc -L . aasdf.c asdfasdf.o -l mylib->libmylib
 			int pd = getpid();//#include ""se le puede meter un path
 			char pid[10];
-			sprintf(pid,"%d\n",pd);
-			while(intentos!=3){
+			
+			while(intentos!=4){
 				if(send(fd,"login:",strlen("login:"),0)<0){
 					printf("Envio login fallido");
 					exit(0);
@@ -85,40 +89,77 @@ int main(int argc, char** argv){
 					exit(0);
 				}
 			
-				printf("Usuario recibido->%s",user);
-			
 				if(send(fd,"passwd:",strlen("passwd:"),0)<0){
                                 	printf("Envio login fallido\n");
                                 	exit(0);
                         	}
-				printf("Passwd enviado\n");
 						
 				if(recv(fd,buffer,BUFFER,0)<0){
                                 	printf("Error recibiendo usuario\n");
                                 	exit(0);
                         	}
-				printf("Contrasenia recibida->%s\n",buffer);
 				
 				if(strcmp("anonymous",strtok(user,"\n"))==0 && strcmp("guest",strtok(buffer,"\n"))==0 ){
+					segmento[1]++;
 					printf("Usuario correcto\n");
-					sprintf(buffer,"%s:$",user);
-					if(send(fd,buffer,strlen(buffer),0)<0){
-                                        	printf("Envio user fallido\n");
-                                        	exit(0);
-                                	}
-					//ej2****************************
-					recv(fd,buffer,BUFFER,0);
-					if(strcmp("whoami",strtok(buffer,"\n"))==0)
-						send(fd,"anonymous\n",strlen("anonymous\n"),0);
-					else if(strcmp("pid",strtok(buffer,"\n"))==0)
-						send(fd,pid,strlen(pid),0);
-						
-				}
+					int rec;
+					int active = 0;
+					while(1){
+						sprintf(buffer,"%s:$",user);
+						if(send(fd,buffer,strlen(buffer),0)<0){
+                                        		printf("Envio user fallido\n");
+                                        		exit(0);
+                                		}
+						if(active==0){
+							active = 1;
+							segmento[3]++;
+						}
+						rec = recv(fd,buffer,BUFFER,0);
+						if(rec==0){
+							segmento[3]--;
+							close(fd);
+							exit(0);
+						}
+						sprintf(pid,"%d\n",pd);
+						if(strcmp("whoami",strtok(buffer,"\n"))==0){
+							send(fd,"anonymous\n",strlen("anonymous\n"),0);
+						}
+						else if(strcmp("pid",strtok(buffer,"\n"))==0){
+							send(fd,pid,strlen(pid),0);
+						}
+						else if(strcmp("quit",strtok(buffer,"\n"))==0){
+							segmento[3]--;
+							close(fd);
+							exit(0);
+						}
+						else if(strcmp("ncnx",strtok(buffer,"\n"))==0){
+							sprintf(buffer,"%d\n",segmento[0]);
+							send(fd,buffer,strlen(buffer),0);
+						}
+						else if(strcmp("nsess",strtok(buffer,"\n"))==0){
+							sprintf(buffer,"%d\n",segmento[1]);
+							send(fd,buffer,strlen(buffer),0);
+						}
+						else if(strcmp("nok",strtok(buffer,"\n"))==0){
+							sprintf(buffer,"%d\n",segmento[2]);
+							send(fd,buffer,strlen(buffer),0);
+						}
+						else if(strcmp("active session",strtok(buffer,"\n"))==0){
+							sprintf(buffer,"%d\n",segmento[3]);
+							send(fd,buffer,strlen(buffer),0);
+						}		
+						else {
+							send(fd,"Error\n",strlen("Error\n"),0);
+						}
+					}
+				}		
 				else{
 					intentos++;
-					printf("Usuario erroneo->%d\n",intentos);
+					segmento[2]++;
+					printf("Intento fallido pid=%d -> %d\n",getpid(),intentos);
 				}
 			}//senial sigchild strtok si en un read de tcp recibo 0 la conexion se ha cerrado
+			//printf("Cerrando por fallos %d",getpid());
 			close(fd);
 			exit(0);		
 		}
