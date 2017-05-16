ae7e7b850af7dd7bf9510bae452cd2eaa79bae61
aitor94 <martinredondoaitor@gmail.com>
2017-05-16 12:07:09 +0200

    Iniciado examen de Julio 2014

---
+++ src/julio2014/client.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/msg.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#define DNI 0x72106540L
+
+
+int main(int argc, char ** argv){
+
+
+}
